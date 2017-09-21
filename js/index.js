a4d7b3aee3851338ba4624689f19d87fda2510f5
晨 <843909098@qq.com>
2017-09-21 22:32:46 +0800

    个人网站制作

--- js/index.js
+++ js/index.js
@@ -1,13 +1,33 @@
 /**
  * Created by ³¿ on 2017/8/20.
  */
-require(['jquery','canvas'],function(){
+require(['jquery','canvas'],function($){
 
     $('#about img').on('click',function(){
         alert('hah');
         alert('you fool');
-    })
+    });
+    var skinBflag=true;
+    $('#skin').on('click',function(){
+        if(skinBflag){
+            $('#skin-left').add(this).css("left","0");
+            $(this).css('left','300px');
+        }else{
+            $('#skin-left').add(this).css("left","-300px");
+            $(this).css('left','0');
+        }
+        skinBflag=!skinBflag;
 
+
+    });
+    var $skinLi=$('#skin-container li');
+    $skinLi.on('click',function(){
+        $(this).addClass('selected').siblings().removeClass('selected');
+    });
+    $('#skin-reset').on('click',function(){
+        $skinLi.removeClass('selected');
+        $skinLi.eq(0).addClass('selected');
+    })
 
 
 
