0f84679299a228f3200c1205a10e2943c284bbd9
晨 <843909098@qq.com>
2017-09-22 15:31:37 +0800

    个人网站制作

--- js/index.js
+++ js/index.js
@@ -1,14 +1,19 @@
 /**
  * Created by ³¿ on 2017/8/20.
  */
-require(['jquery','canvas'],function($){
+require(['jquery','canvas','jquery.cookie'],function($){
+    var $skinLi=$('#skin-container li');
+    var $skin;
+    $skin= $.cookie('skinname');
+    if($.cookie('skinname')==null){
+        $.cookie('skinname','skin1',{expires:30});
+        changeSkin('skin1');
+        $('#skin-container'+' .skin1').addClass('selected');
 
-    $('#about img').on('click',function(){
-        alert('hah');
-        alert('you fool');
-    });
+    }
     var skinBflag=true;
     $('#skin').on('click',function(){
+        $('#skin-container'+' .'+$skin).addClass('selected');
         if(skinBflag){
             $('#skin-left').add(this).css("left","0");
             $(this).css('left','300px');
@@ -20,15 +25,35 @@ require(['jquery','canvas'],function($){
 
 
     });
-    var $skinLi=$('#skin-container li');
+
+
     $skinLi.on('click',function(){
+        var $oldskin=$((".horizontal-line span")).attr('class');
+        $skin=$(this).attr('class').split((/\b/))[0];
+        $.cookie('skinname',$skin,{expires:30});
         $(this).addClass('selected').siblings().removeClass('selected');
+        removeSkin($oldskin);
+        changeSkin($skin);
     });
+    changeSkin($skin);
     $('#skin-reset').on('click',function(){
         $skinLi.removeClass('selected');
         $skinLi.eq(0).addClass('selected');
-    })
+    });
+    function removeSkin($oldskin){
+        $('.horizontal-line span').add(".left-btn").add('.heading-title h4').removeClass($oldskin);
+        $(".down-button a").add(".down-button span").removeClass($oldskin+'-font');
+        $('.heading-title h4').removeClass($oldskin+'a');
 
+
+    }
+    function changeSkin($skin){
+        $('.horizontal-line span').add(".left-btn").addClass($skin);
+        $(".down-button a").add(".down-button span").addClass($skin+'-font');
+        $('.heading-title h4').addClass($skin+'a');
+
+
+    }
 
 
 
